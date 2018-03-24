575e70c7c40ad9c0a31f7e94fcdb81591200ad95
Kyle Lamb <kla98@uclive.ac.nz>
2018-03-24 14:09:23 +1300

    added diffuse lighting that doesnt effect the skybox. However the current cube colour gets applied as the lighting colour on movement/update

--- Assignment.cpp
+++ Assignment.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 #define GL_CLAMP_TO_EDGE 0x812F 
 float cam_angle = 0.0;
-float cam_height = 500.0;
+float cam_height = 20.0;
 float cam_x = 0.0;
 float cam_z = 0.0;
 float cdr=3.14159265/180.0;
@@ -41,7 +41,7 @@ void drawFloor()
 }
 
 void loadGLTextures()				// Load bitmaps And Convert To Textures
-{
+{    
 	glGenTextures(6, texId); 		// Create texture ids
 	// *** left ***
 	glBindTexture(GL_TEXTURE_2D, texId[0]);
@@ -91,7 +91,7 @@ void loadGLTextures()				// Load bitmaps And Convert To Textures
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);	
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
-	glTexEnvi(GL_TEXTURE_ENV,GL_TEXTURE_ENV_MODE,GL_REPLACE);
+	glTexEnvi(GL_TEXTURE_ENV,GL_TEXTURE_ENV_MODE,GL_MODULATE);
 }
 
 void drawSkybox()
@@ -176,11 +176,20 @@ void drawSkybox()
   glEnd();
 }
 
+void drawHouse()
+{
+    glPushMatrix();
+        glColor4f(1.0, 0, 0, 1.0);
+        glTranslatef(0, 5, -50);
+        glutSolidCube(10);
+    glPopMatrix();
+}
+
 void display()  
 {
-	//float lpos[4] = {100., 100., 100., 1.0};  //light's position
+	float lpos[4] = {-40, 50, -80, 1.0};  //light's position
 
-	//glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);    //GL_LINE = Wireframe;   GL_FILL = Solid
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);    //GL_LINE = Wireframe;   GL_FILL = Solid
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
@@ -188,12 +197,13 @@ void display()
     float xdir = -10000.0*sin(cam_angle*cdr);
 	float zdir = -10000.0*cos(cam_angle*cdr);
 	gluLookAt(cam_x, cam_height, cam_z, xdir, cam_height, zdir, 0, 1, 0);
-	//glLightfv(GL_LIGHT0, GL_POSITION, lpos);   //set light position
-
-    //glRotatef(cam_angle, 0.0, 1.0, 0.0);		//rotate the whole scene
+	glLightfv(GL_LIGHT0, GL_POSITION, lpos);   //set light position
 
 	//drawFloor();
+    glDisable(GL_LIGHTING);
     drawSkybox();
+    glEnable(GL_LIGHTING);
+    drawHouse();
 
 	glFlush();
 }
@@ -201,33 +211,45 @@ void display()
 void initialize()
 {
     loadGLTextures();
-	//glClearColor(1.0f, 1.0f, 1.0f, 1.0f);	//Background colour
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);	//Background colour
 
-	//glEnable(GL_LIGHTING);					//Enable OpenGL states
-	//glEnable(GL_LIGHT0);
- 	//glEnable(GL_COLOR_MATERIAL);
+	glEnable(GL_LIGHTING);					//Enable OpenGL states
+	glEnable(GL_LIGHT0);
+    
+    float grey[4] = {0.2, 0.2, 0.2, 1.0};
+    float white[4]  = {1.0, 1.0, 1.0, 1.0};
+    
+    glLightfv(GL_LIGHT0, GL_AMBIENT, grey);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, white);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, white);
+    
+    glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
+    glEnable(GL_COLOR_MATERIAL);
+    
+    glMaterialfv(GL_FRONT, GL_SPECULAR, white);
+    glMaterialf(GL_FRONT, GL_SHININESS, 50);
+    
 	glEnable(GL_DEPTH_TEST);
     glEnable(GL_TEXTURE_2D);
 	glEnable(GL_NORMALIZE);
-    glClearColor (0.0, 0.0, 0.0, 0.0);
-
 
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluPerspective(70, 1, 50, 5000);  //The camera view volume  
+	gluPerspective(70, 1, 1, 5000);  //The camera view volume  
+    
 }
 
 void special(int key, int x, int y)
 {
-    if(key == GLUT_KEY_LEFT) cam_angle++;
-    else if(key == GLUT_KEY_RIGHT) cam_angle--;
+    if(key == GLUT_KEY_LEFT) cam_angle += 2;
+    else if(key == GLUT_KEY_RIGHT) cam_angle -= 2;
 	else if(key == GLUT_KEY_UP) {
-        cam_z -= (10*cos(cam_angle * cdr));
-        cam_x -= (10*sin(cam_angle * cdr));
+        cam_z -= (1*cos(cam_angle * cdr));
+        cam_x -= (1*sin(cam_angle * cdr));
     }
 	else if(key == GLUT_KEY_DOWN){
-        cam_z += (10*cos(cam_angle * cdr));
-        cam_x += (10*sin(cam_angle * cdr));
+        cam_z += (1*cos(cam_angle * cdr));
+        cam_x += (1*sin(cam_angle * cdr));
     }
     
 	glutPostRedisplay();
